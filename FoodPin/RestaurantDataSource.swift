7299b8787080900d97e16657cae052b58caa2317
The Bao <bao.nguyen150028@gmail.com>
2016-11-11 21:39:16 +0700

    Refactoring Restaurant Data Source

--- FoodPin/RestaurantDataSource.swift
+++ FoodPin/RestaurantDataSource.swift
@@ -8,6 +8,54 @@
 
 import Foundation
 import UIKit
-//class RestaurantDataSouce: NSObject, UITableViewDataSource {
-//    
-//}
+import CoreData
+class RestaurantDataSource: NSObject, UITableViewDataSource {
+    private let tableView: UITableView
+
+    let managedObjectcontext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    lazy var resultController: RestaurantFetchResultController = {
+        let controller = RestaurantFetchResultController(managedObjectContext: self.managedObjectcontext, withTableView: self.tableView)
+        return controller
+    }()
+    
+    init(tableView: UITableView) {
+        self.tableView = tableView
+    }
+    func object(at indexPath: IndexPath) -> Restaurant {
+        return resultController.object(at: indexPath)
+    }
+
+    // MARK: - Table view data source
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return resultController.sections?.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resultController.sections?[section].numberOfObjects ?? 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let restaurant = resultController.object(at: indexPath)
+        let restaurantCell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath) as! RestaurantViewCell
+
+        restaurantCell.imgView.image = UIImage(data: restaurant.image as! Data)
+        restaurantCell.nameLabel.text = restaurant.name
+        restaurantCell.locationLabel.text = restaurant.location
+        restaurantCell.typeLabel.text = restaurant.type
+        restaurantCell.imgView?.layer.cornerRadius = 30.0
+        restaurantCell.imgView?.clipsToBounds = true
+
+        return restaurantCell
+    }
+   
+
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+
+    }
+
+  
+}
--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -10,17 +10,15 @@ import UIKit
 import CoreData
 class RestaurantTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
 
-    let managedObjectcontext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
-    lazy var resultController: RestaurantFetchResultController = {
-        let controller = RestaurantFetchResultController(managedObjectContext: self.managedObjectcontext, withTableView: self.tableView)
-        return controller
+    lazy var dataSource: RestaurantDataSource = {
+        return RestaurantDataSource(tableView: self.tableView)
     }()
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        tableView.dataSource = dataSource
         
     }
 
@@ -48,47 +46,17 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
 
 
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return resultController.sections?.count ?? 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return resultController.sections?[section].numberOfObjects ?? 0
-    }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let restaurant = resultController.object(at: indexPath)
-        let restaurantCell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath) as! RestaurantViewCell
-
-        restaurantCell.imgView.image = UIImage(data: restaurant.image as! Data)
-        restaurantCell.nameLabel.text = restaurant.name
-        restaurantCell.locationLabel.text = restaurant.location
-        restaurantCell.typeLabel.text = restaurant.type
-        restaurantCell.imgView?.layer.cornerRadius = 30.0
-        restaurantCell.imgView?.clipsToBounds = true
-        
-        return restaurantCell
-    }
+    
+    // MARK: TableView DELEGATE
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //  TO Detail View Controller
         let detailController = storyboard?.instantiateViewController(withIdentifier: "detailView") as! DetailViewController
-        detailController.restaurant = resultController.object(at: indexPath)
+        detailController.restaurant = dataSource.resultController.object(at: indexPath)
         navigationController?.pushViewController(detailController, animated: true)
     }
-
-
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-
-    }
-
     override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-        
-        let restaurant = resultController.object(at: indexPath)
+
+        let restaurant = dataSource.resultController.object(at: indexPath)
         // Social Sharing Button
         let shareAction = UITableViewRowAction(style: UITableViewRowActionStyle.default, title: "Share", handler: { (action, indexPath) -> Void in
 
@@ -96,6 +64,7 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
 
             if let imageToShare = UIImage(data: restaurant.image as! Data) {
                 let activityController = UIActivityViewController(activityItems: [defaultText, imageToShare], applicationActivities: nil)
+
                 self.present(activityController, animated: true, completion: nil)
             }
         })
@@ -113,7 +82,7 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
 
         return [deleteAction, shareAction]
     }
-   
+
 
 //
 }
